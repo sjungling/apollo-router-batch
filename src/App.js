dd365fe44374d08bdb501f7e12c9208eb2eb6548
Scott Jungling <scott@moderne.io>
2021-11-10 21:02:14 -0800

    chore: working with HttpLink

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+const SAMPLE_QUERY = gql`
+  query test {
+    me {
+      id
+    }
+  }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { data, loading, error } = useQuery(SAMPLE_QUERY);
+  if (error) {
+    console.error(error);
+    return <div>{error.message}</div>;
+  }
+  if (loading) {
+    return <div>Loading</div>;
+  }
+  if (data) {
+    console.log(data);
+    return <div>{data.me.id}</div>;
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloProvider,
+  InMemoryCache,
+  ApolloClient,
+  HttpLink,
+} from "@apollo/client";
+
+import { BatchHttpLink } from "@apollo/client/link/batch-http";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+const batchHttpLink = new BatchHttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
